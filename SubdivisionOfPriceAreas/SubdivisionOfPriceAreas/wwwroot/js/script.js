4f810748ab8f7e57cde9fd65eee5d2860c693ec1
HotdogChamp <dangu19@student.sdu.dk>
2023-03-01 13:52:17 +0100

    Cleaned up comments in script.js file

--- SubdivisionOfPriceAreas/SubdivisionOfPriceAreas/wwwroot/js/script.js
+++ SubdivisionOfPriceAreas/SubdivisionOfPriceAreas/wwwroot/js/script.js
@@ -2,32 +2,14 @@
 
 var editableLayers = new L.FeatureGroup();
 map.addLayer(editableLayers);
-
-//geoJSON feature to add to map with ajax. This can be done better using other scripts/plugin.
-//var geojsonfeature = "../json/geojson.json";
-//var jsonlayer = new l.geojson().addto(map);
-
-//$.ajax({
-//    datatype: "json",
-//    url: geojsonfeature,
-//    success: function (data) {
-//        $(data.features).each(function (key, data) {
-//            jsonlayer.adddata(data);
-//        });
-//    }
-//});
-
-//GeoJSON feature objects. Get them from geojson files that we get with ajax
+ 
 var geojsonLayer = new L.GeoJSON.AJAX("../json/geojson.json");
-/*geojsonLayer.addTo(map);*/
 var geoJsonLayerReplacement = new L.GeoJSON.AJAX("../json/newMap.json");
 
 //layer group to add to map
 var geojsonLayerGroup = new L.LayerGroup();
 geojsonLayerGroup.addTo(map);
 geojsonLayerGroup.addLayer(geojsonLayer);
-
-
 
 //Button to change map
 var buttonAddArea = document.getElementById("addArea");
@@ -39,17 +21,7 @@ function changeMap() {
         geojsonLayerGroup.removeLayer(layer);
     });
 
-    //Add new layer
     geojsonLayerGroup.addLayer(geoJsonLayerReplacement);
-
-    let i = 0;
-    geojsonLayerGroup.eachLayer(function () { i += 1; });
-    console.log('Map has', i, 'layers.');
-
-    //map.eachLayer(function (layer) {
-    //    map.removeLayer(layer);
-    //});
-    //geoJsonLayerReplacement.addTo(map);
 }
 
 //Options for editing via drawing
@@ -85,6 +57,8 @@ map.on(L.Draw.Event.CREATED, function (e) {
         layer.bindPopup('A popup!');
     }
     editableLayers.addLayer(layer);
+
+    //Log GIS coordinates from drawn objects
     console.log("A drawing has been created:")
     console.log(JSON.stringify(layer.toGeoJSON()));
 });
